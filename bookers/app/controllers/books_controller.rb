795ed0b3c82498b994c179d9230a5c2f967cfaa1
Chinami Miyashita <106375669+Natty0404@users.noreply.github.com>
2022-06-11 21:33:06 +0900

    [Finish] 課題2

--- bookers/app/controllers/books_controller.rb
+++ bookers/app/controllers/books_controller.rb
@@ -1,14 +1,19 @@
 class BooksController < ApplicationController
-  def new
+
+   def index
     @book = Book.new
-  end
+    @books = Book.all
+   end
 
   def create
     @book = Book.new(book_params)
-  end
-
-  def index
-    @books = Book.all
+    if @book.save
+       flash[:notice] = "Book was successfully created."
+       redirect_to book_path(@book.id)
+    else
+        @books = Book.all
+        render :index
+    end
   end
 
   def show
@@ -20,9 +25,14 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+     @book = Book.find(params[:id])
+     @book.update(book_params)
+     if @book.save
+       flash[:notice] = "Book was successfully updated."
+       redirect_to book_path(@book.id)
+     else
+       render :edit
+     end
   end
 
   def destroy
